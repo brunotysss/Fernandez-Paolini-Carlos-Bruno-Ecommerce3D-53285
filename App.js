cc492f4c6c95cefb962f2f0de9f050fa0019632a
Carlos Bruno Fernandez Paolini <brunogymnas@gmail.com>
2024-05-24 20:17:13 -0300

    agregando cambio de colores , y el cambio de lanscape a potrait

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View } from "react-native"
+import Home from "./src/screens/Home"
+import { colors } from "./src/constants/colors"
+import Header from "./src/components/Header"
+import ItemListCategory from "./src/screens/ItemListCategory"
+import { useState } from "react"
+import { useFonts } from "expo-font"
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from "react"
+
+const App = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    'Josefin': require('./assets/JosefinSans-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  const [categorySelected, setCategorySelected] = useState("")
+  console.log(categorySelected)
+
+  return (
+    <View style={styles.container}>
+      <Header title={"Decoration 3D"} />
+      {!categorySelected ? (
+        <Home setCategorySelected={setCategorySelected} />
+        ) : (
+        <ItemListCategory categorySelected={categorySelected} setCategorySelected ={setCategorySelected}/>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.teal200,
+  },
+})
+
+export default App
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    teal900: '#393E46',
+    teal600: '#00ADB5',
+    teal400: '#EEEEEE',
+    teal200: '#EEEEEE'
+}
